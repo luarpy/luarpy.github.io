835fec9a7a3ce11c8ccc8a45a9be3f9b5cd826c2
luarpy <biurrun.121721@e.unavarra.es>
2021-11-04 23:32:49 +0100

    This is a commit message

---
+++ toggle.js
@@ -0,0 +1,9 @@
+function setDarkMode() {
+    if (localStorage.getItem('mode') === 'dark') {
+      localStorage.setItem('mode', 'white');
+      document.body.classList.remove('dark-mode');
+    } else {
+      localStorage.setItem('mode', 'dark');
+      document.body.classList.add('dark-mode');
+    }
+  };
