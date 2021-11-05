1e0a6574bbc2ec9e179e8eb03028d40e20e1703c
luarpy <biurrun.121721@e.unavarra.es>
2021-11-05 14:06:20 +0100

    Upstream commit

--- toggle.js
+++ toggle.js
@@ -1,9 +1,4 @@
 function setDarkMode() {
-    if (localStorage.getItem('mode') === 'dark') {
-      localStorage.setItem('mode', 'white');
-      document.body.classList.remove('dark-mode');
-    } else {
-      localStorage.setItem('mode', 'dark');
-      document.body.classList.add('dark-mode');
-    }
-  };
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+  }
